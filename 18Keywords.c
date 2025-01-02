f3e9aa8d3037fb28665019d289eab1af5021e6c7
licheronex <143578251+lichermioneT@users.noreply.github.com>
2025-01-02 14:15:56 +0800

    Create 18Keywords.c

---
+++ 18Keywords.c
@@ -0,0 +1,24 @@
+// 关键字是c语言自带的，关键字自己不能创建
+// 区分大小写
+// 命名不能是关键字
+// 英文和下划线开头,后面可以有下划线
+#include<stdio.h>
+int main()
+{
+
+
+
+    {
+        auto int x = 0;  // 出去就没有了 自动创建 自动销毁 auto
+    }
+
+// 关键字
+    // auto break case char const continue
+    // default do double else enum extern
+    // float for goto if int long register return 
+    // extern short signed sizeof static struct switch
+    // typedef union unsigned void volatile while
+   
+
+    return 0;
+}
