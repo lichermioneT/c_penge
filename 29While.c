ba6e03a88e741d528ee4a5fe067418a0c10e1df3
licheronex <143578251+lichermioneT@users.noreply.github.com>
2025-01-05 10:10:30 +0800

    day5

---
+++ 29While.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int main()
+{
+
+    // int ch = getchar();  // 定义的返回值是整形
+    // printf("%c\n",ch);
+    // putchar(ch);
+
+    // 清理缓冲区的数据
+    // int ch = 0;
+    // while ((ch = getchar()) != EOF)  // getchar是从缓冲区读取数据的 键盘输入的数据在缓冲区
+    // {
+    //     putchar(ch);
+    // }
+    
+    // char password[20] = { 0 };
+    // printf("请输入密码\n");
+    // scanf("%s",password);    // 读取数据
+
+    // // 读取缓冲区里面的数据
+    // int ch = 0;
+    // while ((getchar()) != '\n')
+    // {
+    //     ;                            
+    // }
+    
+    // printf("请确认密码y/n\n");
+    // int ret = getchar();
+    // if('y' == ret)
+    // {
+    //     printf("yes\n");
+    // }
+    // else
+    // {
+    //     printf("no\n");
+    // }
+
+
+    // char ch = '\0';
+    // while ((ch = getchar()) != EOF)
+    // {
+    //     if(ch < '0' || ch > '9')
+    //         continue;
+    //     putchar(ch);  // putchar 0 - 9  打印的是字符
+    // }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
